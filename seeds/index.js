6374dc77bc6dbdb40fb30a2d92289ab58b554cc3
smerill <108644898+smerill@users.noreply.github.com>
2025-06-13 21:02:54 -0300

    basic styling and setup layout/partials
    implemented base styling with bootstrap and modularized layout using ejs-mate, updated checkpoint views and adjusted seed data to generate random images

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const cp = new Checkpoint({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(styles)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At, voluptatum impedit nam maxime faci',
+      price
     });
     await cp.save();
   }
