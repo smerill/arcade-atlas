f5c7e91a96141d5963dcd5710802d3eabe08a7cc
smerill <108644898+smerill@users.noreply.github.com>
2025-06-16 17:06:39 -0300

    error handling and data validation
    add error handling utils, server/client-side validation and error view

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { checkpointSchema } = require('./schemas');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Checkpoint = require('./models/checkpoint');
 
@@ -19,45 +22,66 @@ const app = express();
 app.engine('ejs', ejsMate)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCheckpoint = (req, res, next) => {
+    const { error } = checkpointSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
 // run 'node seeds/index.js' BEFORE acessing these paths to populated the db
 // this ensures you have data to test CRUD op
 
-app.get('/checkpoints', async (req, res) => {
+app.get('/checkpoints', catchAsync(async (req, res) => {
     const checkpoints = await Checkpoint.find({});
     res.render('checkpoints/index.ejs', { checkpoints });
-});
+}));
 
 app.get('/checkpoints/new', (req, res) => {
     res.render('checkpoints/new.ejs');
 });
 
-app.get('/checkpoints/:id', async (req, res) => {
+app.get('/checkpoints/:id', catchAsync(async (req, res) => {
     const checkpoint = await Checkpoint.findById(req.params.id);
     res.render('checkpoints/show.ejs', { checkpoint });
-});
+}));
 
-app.get('/checkpoints/:id/edit', async (req, res) => {
+app.get('/checkpoints/:id/edit', catchAsync(async (req, res) => {
     const checkpoint = await Checkpoint.findById(req.params.id);
     res.render('checkpoints/edit.ejs', { checkpoint });
-});
+}));
 
-app.post('/checkpoints', async (req, res) => {
+app.post('/checkpoints', validateCheckpoint, catchAsync(async (req, res, next) => {
+    // if (!req.body.checkpoint) throw new ExpressError('Invalid Checkpoint Data', 400);
     const checkpoint = new Checkpoint(req.body.checkpoint);
     await checkpoint.save();
     res.redirect(`/checkpoints/${checkpoint._id}`);
-});
+}));
 
-app.patch('/checkpoints/:id', async (req, res) => {
+app.patch('/checkpoints/:id', validateCheckpoint, catchAsync(async (req, res) => {
     const checkpoint = await Checkpoint.findByIdAndUpdate(req.params.id, { ...req.body.checkpoint });
     res.redirect(`/checkpoints/${checkpoint._id}`);
-});
+}));
 
-app.delete('/checkpoints/:id', async (req, res) => {
+app.delete('/checkpoints/:id', catchAsync(async (req, res) => {
     await Checkpoint.findByIdAndDelete(req.params.id);
     res.redirect('/checkpoints');
+}));
+
+app.all(/(.*)/, (req, res, next) => {
+    next(new ExpressError('PAGE NOT FOUND', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!';
+    res.status(status).render('error.ejs', { err });
 });
 
 app.listen(3000, () => {
